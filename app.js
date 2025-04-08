f2f206f493ef6f4e903f027b5b532edb7ead7ece
Reetapatil10 <20reetapatil@gmail>
2025-04-08 12:09:00 +0530

    hii

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ const {Cart} = require('./model/Cart');
 //middleware
 app.use(cors());
 app.use(morgan('dev'))
-app.use(express.json());
+app.use(express.json())
+mongoose.connect('mongodb+srv://20reetapatil:FVcrdSllqKTXWngo@cluster0.jf2ikmd.mongodb.net/?retryWrites=true&w=majority')
 
-mongoose.connect('mongodb://127.0.0.1:27017/rahulEcomm')
+
 .then(()=>{
     console.log("db is connected")
 }).catch((error)=>{
